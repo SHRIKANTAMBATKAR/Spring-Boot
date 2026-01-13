6729a34582d44f47506ddffaa72f6646c12c6c8f
Shrikant Ambatkar <158559554+SHRIKANTAMBATKAR@users.noreply.github.com>
2026-01-13 22:09:11 +0530

    Delete DTO andModelMapper/src/main/java/com/shrikant/employeecrud/dao directory

--- DTO andModelMapper/src/main/java/com/shrikant/employeecrud/dao/EmployeeDao.java
+++
@@ -1,7 +0,0 @@
-package com.shrikant.employeecrud.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.shrikant.employeecrud.entity.Employee;
-
-public interface EmployeeDao extends JpaRepository<Employee, Long> {
-}
